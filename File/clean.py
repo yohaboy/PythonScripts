9e918bfe1eaa7fb83e0d48e9234fc52139ae78ce
yohaboy <yohannesgizaw777@gmail.com>
2025-06-14 00:36:16 -0400

    clean-up

---
+++ File/clean.py
@@ -0,0 +1,13 @@
+import os
+
+folder_path = './my_folder'
+
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        file_path = os.path.join(root, file)
+        if os.path.isfile(file_path) and os.path.getsize(file_path) == 0:
+            os.remove(file_path)
+            print(f'Deleted empty file: {file_path}')
+
+print('Done! All empty files removed.')
+
